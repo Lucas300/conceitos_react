bca2370536db93f4f9301e7a06accb6d3294d384
Lucas300 <ludaniel.sd@gmail.com>
2025-02-12 01:10:36 -0300

    React: Aprendendo sobre props, state, hooks e renderização condicional

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import './App.css'
+import Home from './pages/home/Home'
+import Contador from './pages/contador/Contador'
+import Tarefa from './pages/tarefa/Tarefa';
+import Login from './pages/login/Login';
+
+let titulo: string = 'Dia  mais feliz do homem'; 
+
+function App() {
+
+  return (
+    <>
+      <Login />
+    </>
+  )
+}
+
+export default App
---
+++ src/pages/contador/Contador.tsx
@@ -0,0 +1,25 @@
+﻿import { useState } from "react"
+
+
+function Contador() {
+  const[valor, setValor] = useState(0); //valor = 0 (lembra os metodos get e set)
+
+  function zerar(){
+    setValor(0);
+  }
+
+  function handleClick() {
+    setValor(valor + 1); //valor++  
+  }
+
+  return (
+      <div>
+          <h2>Componente Contador</h2>
+          <p>O valor atual é: {valor}</p>
+          <button onClick={handleClick}>Adicionar 1</button>
+          <button onClick={zerar}>Zerar</button>
+      </div>
+  )
+}
+
+export default Contador
