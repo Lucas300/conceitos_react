fbd7441bd47ab2e60fd164b5b77a4e598df83777
Lucas300 <ludaniel.sd@gmail.com>
2025-02-13 21:46:42 -0300

    Trabalhando com Axios para tratamento das requisições HTTP da nossa API

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,22 @@
 import './App.css'
-import Home from './pages/home/Home'
-import Contador from './pages/contador/Contador'
-import Tarefa from './pages/tarefa/Tarefa';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './pages/home/Home';
 import Login from './pages/login/Login';
-import Gridzao from './pages/gridzao/Gridzao';
-
-let titulo: string = 'Dia  mais feliz do homem'; 
+import { UserProvider } from './contexts/UserContext';
 
 function App() {
 
   return (
-    <>
-      <Gridzao />
-    </>
-  )
+    <UserProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/home" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
+    </UserProvider>
+  );
 }
 
-export default App
+export default App;
